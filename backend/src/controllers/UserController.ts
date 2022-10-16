265212cd3b0cb0a35fe5404a580627aca2ec9451
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2022-10-15 23:38:44 -0300

    Adding a frontend
    adding:
    --frontend
    --controller (backend)
    --services (front and back)

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,3 +1,6 @@
+import { Request, Response } from "express"
+import EmailService from "../services/EmailService"
+
 const users = [
     {name: "Luis Fernando P. B. Pereira", email: "exemplo@gmail.com"}
 ]
@@ -5,5 +8,22 @@ const users = [
 export default {
     async index (req: Request, res: Response){
         return res.json(users)
+    },
+
+    async create(req: Request, res: Response){
+        const emailService = new EmailService();
+        
+        emailService.sendEmail({
+            to: {
+                name: "Fátima",
+                 email: "exemplo2@gmail.com"
+            },
+            message: {
+                subject: "Aprendendo Typescript",
+                 body:"Bem-vindo"
+            }
+        })
+
+        return res.send()
     }
 }
